272a116a37a84824120080502b8bf3e2da654651
Minsoo Cho <textign@gmail.com>
2024-07-18 14:50:49 +0900

    1차 초기 셋팅 수정

--- src/app/(provider)/layout.tsx
+++ src/app/(provider)/layout.tsx
@@ -1,5 +1,5 @@
-import AuthContextProvider from "@/queries/AuthContextProvider";
-import QueryProvider from "@/queries/QueryProvider";
+import AuthContextProvider from "@/providers/AuthContextProvider";
+import QueryProvider from "@/providers/QueryProvider";
 import { PropsWithChildren } from "react";
 
 function ProviderLayout({ children }: PropsWithChildren) {
