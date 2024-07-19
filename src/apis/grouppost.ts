e445e72d19067ac55b32ef033770db059e5cd288
Sol Park <solpark16@gmail.com>
2024-07-19 17:26:03 +0900

    chore : writing form 진행 중

--- src/apis/grouppost.ts
+++ src/apis/grouppost.ts
@@ -1,1 +1,13 @@
-123;
+import { useQuery } from "@tanstack/react-query";
+
+export const getGroupPosts = () => {
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["groupPosts"],
+    queryFn: async () => {
+      const response = await fetch(`/api/grouppost`);
+      const { data } = await response.json();
+      return data;
+    },
+  });
+  return { data, isPending, isError };
+};
--- src/app/api/grouppost/route.ts
+++ src/app/api/grouppost/route.ts
@@ -1,8 +1,12 @@
+import { createClient } from "@/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   //리스트 전체
-  return NextResponse.json("");
+  const supabase = createClient();
+  const { data, error } = await supabase.from("group_posts").select();
+
+  return NextResponse.json({ data });
 }
 
 export async function POST(request: NextRequest) {
