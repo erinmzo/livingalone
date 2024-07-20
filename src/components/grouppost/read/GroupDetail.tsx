d98309f82092da621a7009332d91009bfde5333d
Sol Park <solpark16@gmail.com>
2024-07-20 19:30:01 +0900

    feat : 모달 구현

--- src/components/grouppost/read/GroupDetail.tsx
+++ src/components/grouppost/read/GroupDetail.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import GroupDeleteBtn from "./GroupDeleteBtn";
+import GroupApplyBtn from "./GroupApplyBtn";
 
 type Props = {
   params: { id: string };
@@ -47,7 +48,13 @@ async function GroupDetail({ params }: Props) {
   return (
     <InnerLayout>
       <div>
-        <Image src={img_url} alt="공구템 이미지" width={680} height={500} />
+        <Image
+          src={img_url}
+          alt="공구템 이미지"
+          width={680}
+          height={500}
+          className="hover:"
+        />
         <div>999(하트수)</div>
       </div>
       <div className="flex justify-between">
@@ -66,7 +73,7 @@ async function GroupDetail({ params }: Props) {
         </div>
         <div className="flex">
           <p>달성률</p>
-          {/* 이걸 하려면 신청 받은 갯수도 가져와야한다... */}
+          {/* TODO 이걸 하려면 신청 받은 갯수도 가져와야한다... */}
           <p>80%</p>
         </div>
       </div>
@@ -74,8 +81,8 @@ async function GroupDetail({ params }: Props) {
         <p>마감일 {end_date}까지</p>
         <h5>{title}</h5>
         <p>{price.toLocaleString()}원</p>
-        {/* 글 작성자는 수정삭제, 그 외엔 공구 신청 */}
-        <button>공구 신청하기</button>
+        {/* TODO 글 작성자는 수정삭제, 그 외엔 공구 신청 */}
+        <GroupApplyBtn />
         <div>
           <Link href={`/grouppost/edit/${id}`}>
             <button>글 수정</button>
