2d09091648fff2f2b58e38423ea9fe1ef5282c07
seongbin1013 <plok82890@gmail.com>
2024-07-22 17:03:04 +0900

    fix : Layout 수정

--- src/components/mypage/sideBar/Sidebar.tsx
+++ src/components/mypage/sideBar/Sidebar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"; // Next.js의 Link 컴포넌트를 임포트
+
 function SideBar() {
   return (
     <div className="flex flex-col w-[200px] h-[500px] items-center p-4 border border-gray-400 rounded-lg bg-white">
@@ -6,11 +8,21 @@ function SideBar() {
       <div className="w-full flex-col justify-center items-center ">
         <h2 className="text-md font-bold mb-2">나의 정보</h2>
         <ul className="text-gray-600 space-y-2">
-          <li>찜한 자취템</li>
-          <li>나의 자취템</li>
-          <li>좋아요 공구</li>
-          <li>신청받은 공구</li>
-          <li>내가 쓴 공구</li>
+          <li>
+            <Link href="/mypage/wishmust">찜한 자취템</Link>
+          </li>
+          <li>
+            <Link href="/my-items">나의 자취템</Link>
+          </li>
+          <li>
+            <Link href="/liked-tools">좋아요 공구</Link>
+          </li>
+          <li>
+            <Link href="/mypage/applygroup">신청받은 공구</Link>
+          </li>
+          <li>
+            <Link href="/my">내가 쓴 공구</Link>
+          </li>
         </ul>
       </div>
     </div>
