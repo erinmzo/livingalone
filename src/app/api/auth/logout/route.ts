f1c87371cbde5db451ab748b2ccc3fc9fb1a0a04
t2023-m0020 <t2023-m0020@t2023-m0020ui-MacBookAir.local>
2024-07-23 20:32:47 +0900

    add : 로그인/회원가입 기능 구현

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,5 +1,9 @@
+import { createClient } from "@/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function DELETE() {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signOut();
   return NextResponse.json("");
 }
