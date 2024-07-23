f447bca81f6c6886f850e4df2fb0ed0b7b9ec282
Minsoo Cho <textign@gmail.com>
2024-07-23 12:22:20 +0900

    fix : wish 버튼 css 수정

--- src/components/common/Wish.tsx
+++ src/components/common/Wish.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Wish() {
   return (
-    <button className="absolute right-[20px] top-[20px]">
+    <button>
       <Image src="/img/icon-wish.png" alt="찜하기 버튼" width={32} height={32} />
     </button>
   );
