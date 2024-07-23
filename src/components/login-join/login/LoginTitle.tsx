fe37359c7561bcb517bc0d1600f44efb49b6610c
yuna-hh <ghddbsk12@naver.com>
2024-07-23 23:03:02 +0900

    로그인, 회원가입 페이지 css 수정 완료

--- src/components/login-join/login/LoginTitle.tsx
+++ src/components/login-join/login/LoginTitle.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const LoginTitle = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <h2 className="text-3xl font-semibold text-center">LOGIN</h2>
+    <div className="mb-10">
+      <h2 className="text-[32px] font-bold tracking-tighter text-center">
+        LOGIN
+      </h2>
     </div>
   );
 };
