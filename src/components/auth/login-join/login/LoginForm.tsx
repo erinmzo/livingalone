9ae2b5214163b036aa7e9cf30c5e259a3a4be682
t2023-m0020 <t2023-m0020@t2023-m0020ui-MacBookAir.local>
2024-07-26 20:22:19 +0900

    커밋 테스트

--- src/components/auth/login-join/login/LoginForm.tsx
+++ src/components/auth/login-join/login/LoginForm.tsx
@@ -7,12 +7,14 @@ import { useRouter } from "next/navigation";
 import { Notify, Report } from "notiflix";
 import React, { useState } from "react";
 import Input from "../../common/Input/Input";
+import { createClient } from "@/supabase/client";
 
 const LoginForm = () => {
   const router = useRouter();
   const saveUser = useAuthStore((state) => state.saveUser);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const supabase = createClient();
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -31,7 +33,11 @@ const LoginForm = () => {
     });
 
     if (response.status !== 200) {
-      return Report.failure("로그인에 실패했습니다.", "아이디와 비밀번호를 정확히 입력해 주세요.", "확인");
+      return Report.failure(
+        "로그인에 실패했습니다.",
+        "아이디와 비밀번호를 정확히 입력해 주세요.",
+        "확인"
+      );
     }
 
     const data = await response.json();
@@ -40,9 +46,26 @@ const LoginForm = () => {
     router.push("/");
   };
 
+  const handleGoogleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+    if (data) alert("구글 로그인 중");
+    if (error) console.log("error : ", error);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <form onSubmit={handleLoginSubmit} className="flex flex-col justify-center w-[500px] mb-6">
+      <form
+        onSubmit={handleLoginSubmit}
+        className="flex flex-col justify-center w-[500px] mb-6"
+      >
         <div className="flex flex-col mb-6">
           <Input
             label="이메일"
@@ -61,7 +84,9 @@ const LoginForm = () => {
             onChange={handlePasswordChange}
           />
         </div>
-        <button className="py-3 text-xl bg-black text-white rounded-lg">로그인</button>
+        <button className="py-3 text-xl bg-black text-white rounded-lg">
+          로그인
+        </button>
       </form>
       <div className="flex flex-col items-center gap-6 w-[500px]">
         <Link href="/join">
@@ -69,8 +94,17 @@ const LoginForm = () => {
             회원가입
           </button>
         </Link>
-        <button className="flex items-center justify-center w-[500px] py-2 text-xl border-2 border-[#000] rounded-lg font-medium">
-          <Image src="/img/icon-google.png" alt="구글 로그인 아이콘" width={32} height={32} className="mr-2" />
+        <button
+          className="flex items-center justify-center w-[500px] py-2 text-xl border-2 border-[#000] rounded-lg font-medium"
+          onClick={handleGoogleLogin}
+        >
+          <Image
+            src="/img/icon-google.png"
+            alt="구글 로그인 아이콘"
+            width={32}
+            height={32}
+            className="mr-2"
+          />
           구글 간편로그인
         </button>
       </div>
