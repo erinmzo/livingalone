bc713dc02265cdad32b5d5b714314a9122a33027
yuna-hh <ghddbsk12@naver.com>
2024-07-27 16:48:13 +0900

    pull

--- src/components/auth/login-join/login/LoginForm.tsx
+++ src/components/auth/login-join/login/LoginForm.tsx
@@ -6,12 +6,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Notify, Report } from "notiflix";
 import React, { useState } from "react";
-<<<<<<< HEAD
-import Input from "../../common/Input/Input";
-import { createClient } from "@/supabase/client";
-=======
 import Input from "../../common/Input";
->>>>>>> 453d8bdaf689ae9d226c8e1c114c30168d643114
+import { createClient } from "@/supabase/client";
 
 const LoginForm = () => {
   const router = useRouter();
@@ -98,14 +94,10 @@ const LoginForm = () => {
             회원가입
           </button>
         </Link>
-<<<<<<< HEAD
         <button
           className="flex items-center justify-center w-[500px] py-2 text-xl border-2 border-[#000] rounded-lg font-medium"
           onClick={handleGoogleLogin}
         >
-=======
-        <button className="flex items-center justify-center w-[500px] py-2 text-xl border-2 border-[#000] rounded-lg font-medium">
->>>>>>> 453d8bdaf689ae9d226c8e1c114c30168d643114
           <Image
             src="/img/icon-google.png"
             alt="구글 로그인 아이콘"
