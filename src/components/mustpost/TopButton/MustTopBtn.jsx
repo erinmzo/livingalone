e0e3b3574d5a3d270cdf4fcf89de81ef76f68fc5
yuna-hh <ghddbsk12@naver.com>
2024-07-29 16:14:12 +0900

    must top button fixed 버전 완료

--- src/components/mustpost/TopButton/MustTopBtn.jsx
+++ src/components/mustpost/TopButton/MustTopBtn.jsx
@@ -12,8 +12,10 @@ function MustTopBtn() {
   return (
     <button
       onClick={topBtn}
+      // className="inline-flex justify-center items-center
+      //   sticky left-full p-[17px] border-2 border-[#899490] rounded-full"
       className="inline-flex justify-center items-center
-        fixed left-full p-[17px] border-2 border-[#899490] rounded-full"
+        btn-fixed-right"
     >
       <Image
         src="/img/icon-top-button.png"
@@ -28,5 +30,6 @@ function MustTopBtn() {
 export default MustTopBtn;
 
 // 질문할것
-// - 일단 지금은 컴포넌트 아래쪽으로 둬서 누르면 올라가게끔 만들어놨는데 이걸 의도한게 맞을지
-// 아니면 전체 스크롤에 따라붙을것인지
+// - 1.일단 지금은 컴포넌트 아래쪽으로 둬서 누르면 올라가게끔 만들어놨는데 이걸 의도한게 맞을지
+// 2. 아니면 전체 스크롤에 따라붙을것인지
+// 일단 1이 맞다면 sticky로 해도 괜찮기 때문에 그대로 가야하고 2가 맞다면 fixed로 바꿔야함 ㅠㅠ
