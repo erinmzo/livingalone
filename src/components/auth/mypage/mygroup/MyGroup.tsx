0744de9750b7f577a2a0040ef3211682829a4e4f
Minsoo Cho <textign@gmail.com>
2024-07-30 20:14:44 +0900

    pull

--- src/components/auth/mypage/mygroup/MyGroup.tsx
+++ src/components/auth/mypage/mygroup/MyGroup.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import { getMyGroupPosts } from "@/apis/mypage";
+import { TMyGroupPost } from "@/types/types";
 import { useAuthStore } from "@/zustand/authStore";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import MyGroupPost from "./MyGroupPost";
-import { GroupApplication, GroupPost, TMyGroupPost } from "@/types/types";
 
 function MyGroup() {
   const user = useAuthStore((state) => state.user);
 
-  // TODO useQuery 타입 지정해주기
   const {
     data: groupPosts,
     isPending,
@@ -25,10 +25,13 @@ function MyGroup() {
   });
 
   if (isPending)
-    return <div className="flex justify-center items-center">로딩중...</div>;
+    return (
+      <div className="flex justify-center items-center">
+        <Image src="/img/loading-spinner.svg" alt="로딩중" width={200} height={200} />
+      </div>
+    );
 
-  if (isError)
-    return <div className="flex justify-center items-center">에러...</div>;
+  if (isError) return <div className="flex justify-center items-center">에러...</div>;
   return (
     <div className="flex-col">
       <h5 className="font-bold text-[24px] mb-[32px] w-full">나의 정보</h5>
