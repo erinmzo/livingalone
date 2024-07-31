06271d77802ab3e0cd5a7c3bb4f8de9d42c879ca
Minsoo Cho <textign@gmail.com>
2024-07-31 12:10:34 +0900

    image upload caching

--- src/app/api/auth/profile/image/route.ts
+++ src/app/api/auth/profile/image/route.ts
@@ -7,9 +7,10 @@ export async function POST(request: NextRequest) {
   const newProfileImage: any = formData.get("file");
   try {
     const supabase = createClient();
-    const { data } = await supabase.storage
-      .from("profile")
-      .upload(`profile_${Date.now()}.png`, newProfileImage);
+    const { data } = await supabase.storage.from("profile").upload(`profile_${Date.now()}.png`, newProfileImage, {
+      cacheControl: "600",
+      upsert: false,
+    });
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: "데이터를 등록하는 데 실패했습니다." });
