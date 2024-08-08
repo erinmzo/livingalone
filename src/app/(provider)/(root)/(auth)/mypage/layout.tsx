4307f26013de9d98009907ffd3aca936ddaa9a25
seongbin1013 <plok82890@gmail.com>
2024-08-08 19:44:27 +0900

    update: MobileSideBar 생성

--- src/app/(provider)/(root)/(auth)/mypage/layout.tsx
+++ src/app/(provider)/(root)/(auth)/mypage/layout.tsx
@@ -1,19 +1,28 @@
+"use client";
+
+import MobileSideBar from "@/components/auth/mypage/sideBar/MobileSideBar";
 import SideBar from "@/components/auth/mypage/sideBar/Sidebar";
 import Page from "@/components/common/Page/Page";
 import MobileHeader from "@/components/common/header/MobileHeader";
 import MobileNav from "@/components/common/header/MobileNav";
 import IsOpenProvider from "@/providers/IsOpenProvider";
+import { useIsOpen } from "@/zustand/isOpenStore";
 import { PropsWithChildren } from "react";
 
 function MyPageLayout({ children }: PropsWithChildren) {
+  const isOpenSideBar = useIsOpen((state) => state.isOpenSideBar);
+
   return (
     <>
       <MobileHeader hamburger title="마이페이지" />
       <IsOpenProvider>
         <Page>
-          <h1 className="text-[32px] text-center font-bold hidden md:block">마이페이지</h1>
+          <h1 className="text-[32px] text-center font-bold hidden md:block">
+            마이페이지
+          </h1>
           <div className="flex py-[100px] px-[50px]">
             <SideBar />
+            {isOpenSideBar && <MobileSideBar />}
             <main className="ml-[50px] w-auto grow">{children}</main>
           </div>
         </Page>
