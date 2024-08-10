1c4a1caeb08f81733c9b6b88d950c00ba265593b
yuna-hh <ghddbsk12@naver.com>
2024-08-10 15:15:21 +0900

    pull

--- src/components/mustpost/list/MustList.tsx
+++ src/components/mustpost/list/MustList.tsx
@@ -108,8 +108,20 @@ function MustList() {
           </div>
         </div>
       ) : (
-        <div className="min-h-screen flex justify-center font-bold text-gray-3">
-          해당 카테고리에 맞는 게시글이 없습니다.
+        <div className="min-h-[400px] md:min-h-screen flex justify-center">
+          <div className="flex flex-col justify-center items-center mb-[64px]">
+            <div className="relative w-[67px] md:w-[100px] h-[62px] md:h-[94px] mb-5">
+              <Image
+                src="/img/icon-empty.png"
+                alt="empty"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <h4 className="text-gray-2 text-[16px] mb-1">
+              해당 카테고리에 맞는 게시글이 없습니다.
+            </h4>
+          </div>
         </div>
       )}
     </div>
