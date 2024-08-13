6c6dd0ce3be2f0b002c39ef93423370e33f1af57
seongbin1013 <plok82890@gmail.com>
2024-08-13 15:04:38 +0900

    fix: 자취템 스켈레톤 수정

--- src/components/auth/mypage/wishMust/SkeletonMust.tsx
+++ src/components/auth/mypage/wishMust/SkeletonMust.tsx
@@ -3,10 +3,9 @@ import React from "react";
 const SkeletonMust = () => {
   return (
     <div className="relative animate-pulse">
-      <div
-        className="font-bold mt-8 md:mt-0 text-[12px] flex items-center justify-center border border-main-8 rounded-full w-[76px] h-[30px] text-main-8 
-          md:border-none md:rounded-none md:text-left md:text-black md:text-[24px] md:w-[115px] md:h-[29px] md:block"
-      ></div>
+      <div className="flex items-center justify-center md:block">
+        <div className=" bg-gray-2 md:rounded mb-6 mt-8 md:mt-0 border rounded-full w-[76px] h-[30px] md:w-[115px] md:h-[29px]"></div>
+      </div>
       <div className="overflow-hidden relative aspect-square rounded-lg border border-gray-2 bg-gray-2 w-[315px]"></div>
       <div className="px-1 mt-4 w-[309px]">
         <span className="block text-[14px] bg-gray-2 h-4 rounded  mb-1"></span>
