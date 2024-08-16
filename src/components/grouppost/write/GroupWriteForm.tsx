6223e350878bc454918409fb35ecf3265969d299
Sol Park <solpark16@gmail.com>
2024-08-16 12:22:09 +0900

    style: 글 작성 시 아래 버튼 안 보이는 오류 수정 중

--- src/components/grouppost/write/GroupWriteForm.tsx
+++ src/components/grouppost/write/GroupWriteForm.tsx
@@ -144,145 +144,147 @@ function GroupWriteForm() {
 
   return (
     <InnerLayout>
-      <GroupPostNotice checkBox={checkBox} setCheckBox={setCheckBox} />
+      <div className="pb-[76px] md:pb-0">
+        <GroupPostNotice checkBox={checkBox} setCheckBox={setCheckBox} />
 
-      <div className="flex flex-col gap-3 md:gap-5">
-        <InputField
-          labelName="제목"
-          name="title"
-          type="text"
-          value={title}
-          placeHolder="제목을 입력해주세요"
-          minLength={1}
-          onchangeValue={onChangeInput}
-          error={error.titleError}
-        />
-        <div className="flex gap-2 md:gap-[41px]">
-          <div className="flex gap-[2px]">
-            <label
-              htmlFor="endDate"
-              className="hidden md:flex flex-0 w-[70px] md:w-[78px] h-[38px] items-center md:text-[18px] text-gray-4"
-            >
-              공구기간
-            </label>
-            <label className="flex md:hidden flex-0 w-[70px] md:w-[78px] h-[38px] items-center md:text-[18px] text-gray-4">
-              마감일
-            </label>
-            <div className="flex gap-2">
-              <label className="hidden h-[38px] md:flex items-center text-[14px] text-black">
+        <div className="flex flex-col gap-3 md:gap-5">
+          <InputField
+            labelName="제목"
+            name="title"
+            type="text"
+            value={title}
+            placeHolder="제목을 입력해주세요"
+            minLength={1}
+            onchangeValue={onChangeInput}
+            error={error.titleError}
+          />
+          <div className="flex gap-2 md:gap-[41px]">
+            <div className="flex gap-[2px]">
+              <label
+                htmlFor="endDate"
+                className="hidden md:flex flex-0 w-[70px] md:w-[78px] h-[38px] items-center md:text-[18px] text-gray-4"
+              >
+                공구기간
+              </label>
+              <label className="flex md:hidden flex-0 w-[70px] md:w-[78px] h-[38px] items-center md:text-[18px] text-gray-4">
                 마감일
+              </label>
+              <div className="flex gap-2">
+                <label className="hidden h-[38px] md:flex items-center text-[14px] text-black">
+                  마감일
+                </label>
+                <div>
+                  <input
+                    id="endDate"
+                    name="endDate"
+                    type="date"
+                    value={endDate}
+                    onChange={onChangeInput}
+                    className="rounded-none border-b-[1px] border-gray-3 py-2 px-[2px] md:text-[18px] font-bold text-black outline-none"
+                  />
+                  {error.endDateError && (
+                    <p className={`text-red-3 text-[12px] mt-2`}>
+                      {error.endDateError}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-2 overflow-hidden">
+              <label
+                htmlFor="peopleNum"
+                className="flex-0 shrink-0 w-[70px] md:w-[78px] h-[38px] flex items-center md:text-[18px] text-gray-4"
+              >
+                공구인원
               </label>
               <div>
                 <input
-                  id="endDate"
-                  name="endDate"
-                  type="date"
-                  value={endDate}
+                  id="peopleNum"
+                  name="peopleNum"
+                  type="number"
+                  placeholder="숫자만 입력해주세요."
+                  value={peopleNum}
                   onChange={onChangeInput}
-                  className="rounded-none border-b-[1px] border-gray-3 py-2 px-[2px] md:text-[18px] font-bold text-black outline-none"
+                  className="rounded-none w-auto max-w-[83px] md:w-[100px] pl-[2px] px-[2px] py-2 border-b border-gray-3 md:text-[18px] font-bold text-black outline-none"
                 />
-                {error.endDateError && (
+                {error.peopleNumError && (
                   <p className={`text-red-3 text-[12px] mt-2`}>
-                    {error.endDateError}
+                    {error.peopleNumError}
                   </p>
                 )}
               </div>
             </div>
           </div>
 
-          <div className="flex gap-2 overflow-hidden">
+          <InputField
+            labelName="상품이름"
+            name="item"
+            type="text"
+            value={item}
+            placeHolder="제품명을 입력해주세요."
+            minLength={1}
+            onchangeValue={onChangeInput}
+            error={error.itemError}
+          />
+          <InputField
+            labelName="공구가격"
+            name="price"
+            type="number"
+            value={price}
+            placeHolder="숫자만 입력해주세요."
+            minLength={1}
+            onchangeValue={onChangeInput}
+          />
+          <InputField
+            labelName="상품링크"
+            name="link"
+            type="text"
+            value={link}
+            placeHolder="(선택사항) 상품소개 페이지 링크를 넣어주세요."
+            minLength={1}
+            onchangeValue={onChangeInput}
+          />
+          <div className="ml-[70px] md:ml-[78px] flex flex-col md:flex-row gap-2 md:gap-4 items-start mb-[6px]">
+            <input
+              className="hidden"
+              id="image-file"
+              type="file"
+              onChange={addImageHandler}
+            />
             <label
-              htmlFor="peopleNum"
-              className="flex-0 shrink-0 w-[70px] md:w-[78px] h-[38px] flex items-center md:text-[18px] text-gray-4"
+              className="flex justify-center items-center px-7 py-[7px] border border-gray-4 bg-gray-1 font-bold text-[12px] text-gray-4 rounded-full cursor-pointer"
+              htmlFor="image-file"
             >
-              공구인원
+              {imgUrl ? "이미지 수정" : "이미지 업로드"}
             </label>
-            <div>
-              <input
-                id="peopleNum"
-                name="peopleNum"
-                type="number"
-                placeholder="숫자만 입력해주세요."
-                value={peopleNum}
-                onChange={onChangeInput}
-                className="rounded-none w-auto max-w-[83px] md:w-[100px] pl-[2px] px-[2px] py-2 border-b border-gray-3 md:text-[18px] font-bold text-black outline-none"
+            {error.imageUrlError && (
+              <p className={`text-red-3 text-[12px] mt-2`}>
+                {error.imageUrlError}
+              </p>
+            )}
+            {imgUrl && (
+              <Image
+                src={imgUrl}
+                alt="선택한 이미지"
+                width={200}
+                height={200}
+                className="border md:border-none rounded-[4px] md:rounded-none border-gray-3 w-[44px] h-[44px] md:w-[200px] md:h-auto object-cover"
               />
-              {error.peopleNumError && (
-                <p className={`text-red-3 text-[12px] mt-2`}>
-                  {error.peopleNumError}
-                </p>
-              )}
-            </div>
+            )}
           </div>
         </div>
-
-        <InputField
-          labelName="상품이름"
-          name="item"
-          type="text"
-          value={item}
-          placeHolder="제품명을 입력해주세요."
-          minLength={1}
-          onchangeValue={onChangeInput}
-          error={error.itemError}
-        />
-        <InputField
-          labelName="공구가격"
-          name="price"
-          type="number"
-          value={price}
-          placeHolder="숫자만 입력해주세요."
-          minLength={1}
-          onchangeValue={onChangeInput}
-        />
-        <InputField
-          labelName="상품링크"
-          name="link"
-          type="text"
-          value={link}
-          placeHolder="(선택사항) 상품소개 페이지 링크를 넣어주세요."
-          minLength={1}
-          onchangeValue={onChangeInput}
-        />
-        <div className="ml-[70px] md:ml-[78px] flex flex-col md:flex-row gap-2 md:gap-4 items-start mb-[6px]">
-          <input
-            className="hidden"
-            id="image-file"
-            type="file"
-            onChange={addImageHandler}
-          />
-          <label
-            className="flex justify-center items-center px-7 py-[7px] border border-gray-4 bg-gray-1 font-bold text-[12px] text-gray-4 rounded-full cursor-pointer"
-            htmlFor="image-file"
-          >
-            {imgUrl ? "이미지 수정" : "이미지 업로드"}
-          </label>
-          {error.imageUrlError && (
-            <p className={`text-red-3 text-[12px] mt-2`}>
-              {error.imageUrlError}
-            </p>
-          )}
-          {imgUrl && (
-            <Image
-              src={imgUrl}
-              alt="선택한 이미지"
-              width={200}
-              height={200}
-              className="border md:border-none rounded-[4px] md:rounded-none border-gray-3 w-[44px] h-[44px] md:w-[200px] md:h-auto object-cover"
-            />
-          )}
+        <div className="mt-[14px]">
+          <EditorModule editorRef={editorRef} />
         </div>
-      </div>
-      <div className="mt-[14px]">
-        <EditorModule editorRef={editorRef} />
-      </div>
-      <div className="flex justify-center pb-[123px] md:pb-0 ">
-        <button
-          className="bg-main-8 w-full md:w-[300px] py-[10px] text-white rounded-full font-bold text-[20px] mt-6 md:mt-[64px]"
-          onClick={addGroupPostHandler}
-        >
-          등록하기
-        </button>
+        <div className="flex justify-center pb-[123px] md:pb-0 ">
+          <button
+            className="bg-main-8 w-full md:w-[300px] py-[10px] text-white rounded-full font-bold text-[20px] mt-6 md:mt-[64px]"
+            onClick={addGroupPostHandler}
+          >
+            등록하기
+          </button>
+        </div>
       </div>
     </InnerLayout>
   );
