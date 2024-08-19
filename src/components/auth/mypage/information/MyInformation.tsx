97a9f74023438cd67e46040acecb8d668e8f7e7e
seongbin1013 <plok82890@gmail.com>
2024-08-19 10:31:04 +0900

    fix: 마이페이지 나의 정보 Input

--- src/components/auth/mypage/information/MyInformation.tsx
+++ src/components/auth/mypage/information/MyInformation.tsx
@@ -94,7 +94,11 @@ function MyInformation() {
         const fileType = file.type;
 
         if (fileType !== "image/jpeg" && fileType !== "image/png") {
-          Report.warning("유효하지 않은 파일 형식", "JPG 또는 PNG 파일만 업로드 가능합니다.", "확인");
+          Report.warning(
+            "유효하지 않은 파일 형식",
+            "JPG 또는 PNG 파일만 업로드 가능합니다.",
+            "확인"
+          );
           return;
         }
 
@@ -131,9 +135,16 @@ function MyInformation() {
       return Report.info("변경된 내용이 없습니다.", "", "확인");
     }
 
-    if (nickname !== profile?.nickname && nickname.trim() === "" && !imgFile && !detailAddress && !address) {
+    if (
+      nickname !== profile?.nickname &&
+      nickname.trim() === "" &&
+      !imgFile &&
+      !detailAddress &&
+      !address
+    ) {
       return Report.warning("닉네임 공백", "닉네임을 적어주세요!", "확인");
     }
+
     if (nickname.length < 2) {
       return Report.info("닉네임 길이", "2글자 이상으로 작성해주세요", "확인");
     } else if (nickname.length > 8) {
@@ -168,7 +179,9 @@ function MyInformation() {
                 />
               )}
             </div>
-            <div className="text-[16px] font-bold md:hidden text-center w-full h-[19px]">{profile?.nickname}</div>
+            <div className="text-[16px] font-bold md:hidden text-center w-full h-[19px]">
+              {profile?.nickname}
+            </div>
           </div>
         </div>
         <form className="flex flex-col items-center w-full">
@@ -177,8 +190,8 @@ function MyInformation() {
               <Input
                 variant="default"
                 label="닉네임"
-                placeholder={profile?.nickname}
-                value={nickname ?? profile?.nickname}
+                // placeholder={profile?.nickname}
+                value={nickname || profile?.nickname || "혼살러"}
                 name="nickname"
                 onChange={onChangeInput}
               />
@@ -200,26 +213,28 @@ function MyInformation() {
               className="flex gap-3 w-fit py-2 px-4 border border-gray-3 bg-white font-bold rounded-full md:mb-3 mb-2 justify-center items-center"
               onClick={handleSearchAddress}
             >
-              <span className="text-center md:text-[12px] text-[16px] text-gray-3">주소검색</span>
+              <span className="text-center md:text-[12px] text-[16px] text-gray-3">
+                주소검색
+              </span>
             </button>
             {isPostModalOpen && (
-              <div className="absolute left-0 top-[48px] border border-black">
+              <div className="absolute left-0 top-[48px] border border-black z-[999] ">
                 <DaumPostcode onComplete={onCompleteAddress}></DaumPostcode>
               </div>
             )}
             <div className="flex flex-col gap-2">
               <Input
                 variant="underline"
-                value={address}
-                placeholder={profile?.address! || "OO시 OO구  OO동"}
+                value={address || profile?.address! || "OO시 OO구  OO동"}
+                // placeholder={profile?.address! || "OO시 OO구  OO동"}
                 readOnly
               />
               <Input
                 variant="underline"
-                value={detailAddress}
+                value={detailAddress || profile?.detail_address || "OO호"}
                 name="detailAddress"
                 onChange={onChangeInput}
-                placeholder={profile?.detail_address! || "OO호"}
+                // placeholder={profile?.detail_address! || "OO호"}
               />
             </div>
           </div>
