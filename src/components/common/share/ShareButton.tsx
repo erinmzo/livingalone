040a227c07be9035989a4dfc92505a4bfb6729da
Sol Park <solpark16@gmail.com>
2024-08-19 21:01:35 +0900

    style: 공구템 버튼 모바일 디자인 변경

--- src/components/common/share/ShareButton.tsx
+++ src/components/common/share/ShareButton.tsx
@@ -22,14 +22,14 @@ function ShareButton({
         onClick={() => {
           setIsModalOpen(true);
         }}
-        className="shrink-0 w-[44px] md:w-[100px] h-[44px] md:h-auto py-[10px] flex items-center justify-center gap-[6px] bg-gray-1 border border-gray-3 rounded-full"
+        className="shrink-0 w-[32px] md:w-[100px] h-[32px] md:h-auto md:py-[10px] flex items-center justify-center gap-[6px] bg-gray-1 border border-gray-3 rounded-full"
       >
         <Image
           src="/img/icon-share.png"
           alt="공유 이미지"
           width={16}
           height={22}
-          className="w-[16px] h-[22px]"
+          className="w-[13px] md:w-[15px] h-[18px] md:h-[20px]"
         />
         <p className="text-gray-3 text-[20px] hidden md:block">공유</p>
       </button>
