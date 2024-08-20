94e340530180c2db296689d4c37dfe13e2a21471
yuna-hh <ghddbsk12@naver.com>
2024-08-20 09:54:55 +0900

    pull

--- src/components/mustpost/write/SelectCategory.tsx
+++ src/components/mustpost/write/SelectCategory.tsx
@@ -11,7 +11,11 @@ interface SelectCategoryProps {
   error: string;
 }
 
-function SelectCategory({ initialCategoryName, selectCategory, error }: SelectCategoryProps) {
+function SelectCategory({
+  initialCategoryName,
+  selectCategory,
+  error,
+}: SelectCategoryProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const {
@@ -67,11 +71,13 @@ function SelectCategory({ initialCategoryName, selectCategory, error }: SelectCa
               />
             </button>
             {isOpen && (
-              <ul className="z-[9999] flex flex-col items-start justify-center absolute left-0 w-[100px] border-t-0 border border-gray-4 bg-white py-[4px]">
+              <ul className="z-50 flex flex-col items-start justify-center absolute left-0 w-[100px] border-t-0 border border-gray-4 bg-white py-[4px]">
                 {mustCategories?.map((category) => (
-                  <li key={category.id} className="w-full text-gray-4 text-[14px] hover:bg-main-2">
+                  <li
+                    key={category.id}
+                    className="w-full text-gray-4 text-[14px] hover:bg-main-2"
+                  >
                     <button
-                      // onClick={() => handleSelectCategory(category.name)}
                       onClick={() => handleSelectCategory(category)}
                       className="inline-flex w-full py-[5px] px-[16px]  hover:bg-main-2"
                     >
