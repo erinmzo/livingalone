e83a281df089a257acebe2d11bd240a5cab6ed10
Sol Park <solpark16@gmail.com>
2024-08-20 19:35:17 +0900

    chore: console.log 삭제

--- src/components/auth/mypage/information/MyInformation.tsx
+++ src/components/auth/mypage/information/MyInformation.tsx
@@ -141,7 +141,6 @@ function MyInformation() {
     ) {
       return Report.warning("닉네임 공백", "닉네임을 적어주세요!", "확인");
     }
-    console.log(nickname);
     if (nickname.length < 2 || nickname.length > 8) {
       return Report.info(
         "닉네임 길이",
